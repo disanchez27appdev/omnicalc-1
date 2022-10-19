90d092843e2a4096a023ae7e23f227946f35fda4
disanchez27edu <disanchez@uchicago.edu>
2022-10-19 18:26:45 +0000

    first_calc_done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,15 @@ class ApplicationController < ActionController::Base
 def blank_square_form
 
   render ({ :template => "calculation_templates/square_form.html.erb"})
-  
+
+end
+
+def blank_square_results
+
+  @num = params.fetch("user_input").to_f
+  @square= @num ** 2
+  render ({ :template => "calculation_templates/square_form_results.html.erb"})
+
 end
   
 end
