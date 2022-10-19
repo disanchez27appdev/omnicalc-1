d76e96aa7ba79686bd2a89e7b590aa9fd1bc38bf
disanchez27edu <disanchez@uchicago.edu>
2022-10-19 22:43:27 +0000

    day1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,59 @@
 class ApplicationController < ActionController::Base
 
-def blank_square_form
+  def index
+    render ({ :template => "calculation_templates/square_form.html.erb"})
+  end
 
+  def blank_square_form
   render ({ :template => "calculation_templates/square_form.html.erb"})
-
 end
 
 def blank_square_results
-
   @num = params.fetch("user_input").to_f
   @square= @num ** 2
-  render ({ :template => "calculation_templates/square_form_results.html.erb"})
 
+  render ({ :template => "calculation_templates/square_form_results.html.erb"})
+end
+
+def blank_square_root_form
+  render ({ :template => "calculation_templates/square_root_form.html.erb"})
+end
+
+def blank_square_root_results
+  @num = params.fetch("user_input").to_f
+  @square_root= @num ** 0.5
+
+  render ({ :template => "calculation_templates/square_root_form_results.html.erb"})
+end
+
+def blank_payment_form
+  render ({ :template => "calculation_templates/payment_form.html.erb"})
+end
+
+
+def blank_payment_results
+  @present_value      = params.fetch("principal_input").to_f
+  @rate               = params.fetch("apr_input").to_f/100
+  @number_of_periods  = params.fetch("periods_input").to_f
+
+  @numerator = @present_value.to_f*@rate.to_f/12
+  @denominator = (1-((1+(@rate.to_f/12)) ** (@number_of_periods.to_f*-12)).to_f)
+  @payment = @numerator.to_f/@denominator.to_f
+
+  render ({ :template => "calculation_templates/payment_form_results.html.erb"})
+end
+
+def blank_random_form
+  render ({ :template => "calculation_templates/random_form.html.erb"})
+end
+
+def blank_random_results
+  @min = params.fetch("min_input").to_f
+  @max = params.fetch("max_input").to_f
+  @rand = Kernel.rand(@max.to_f).to_f + Kernel.rand() + (@min-1)
+  
+
+  render ({ :template => "calculation_templates/random_form_results.html.erb"})
 end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,21 @@
 Rails.application.routes.draw do
 
+get("", {:controller => "application", :action => "index"}) 
+
 get("/square/new", {:controller => "application", :action => "blank_square_form"})  
 
-get("/square/results", {:controller => "application", :action => "blank_square_results"})  
+get("/square/results", {:controller => "application", :action => "blank_square_results"}) 
+
+get("/square_root/new", {:controller => "application", :action => "blank_square_root_form"})
+
+get("/square_root/results", {:controller => "application", :action => "blank_square_root_results"})
+
+get("/payment/new", {:controller => "application", :action => "blank_payment_form"})
+
+get("/payment/results", {:controller => "application", :action => "blank_payment_results"})
+
+get("/random/new", {:controller => "application", :action => "blank_random_form"})
+
+get("/random/results", {:controller => "application", :action => "blank_random_results"})
 
 end
